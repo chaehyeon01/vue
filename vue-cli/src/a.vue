42d6b48438777e59a1718046e884cd8e2a4c9499
NamChaeHyeon <namchae01@makebot.ai>
2021-01-26 19:34:14 +0900

    vue_기초강의완강

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- html -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+// 자바스크립트 - 인스턴스 옵션
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+/* css */
+</style>
